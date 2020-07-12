d77b988cbd78400230103d39da866c11f190c9f7
Thibault Wittemberg <twittemberg@genetec.com>
2020-07-12 16:06:24 -0400

    xcc: bump to 0.1.4

--- Formula/xcodecoverageconverter.rb
+++ Formula/xcodecoverageconverter.rb
@@ -5,11 +5,11 @@ class Xcodecoverageconverter < Formula
   desc ""
   homepage "https://github.com/twittemb/XcodeCoverageConverter"
   url "https://github.com/twittemb/XcodeCoverageConverter.git",
-      :tag => "0.1.3", :revision => "5830940ad9bdd463052ce0331ac74650220c9012"
+      :tag => "0.1.4", :revision => "8d3965ca31ab5e9fa61fee143b2d49cae28c3eff"
   head "https://github.com/twittemb/XcodeCoverageConverter.git"
 
   depends_on :xcode => ["11.0", :build] 
-  version "0.1.3"
+  version "0.1.4"
   sha256 ""
   license "MIT"
 
